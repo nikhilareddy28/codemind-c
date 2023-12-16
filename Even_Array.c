5adc2debaf39faa287b9569de5b721b79628790e
nikhilareddy28 <147791755+nikhilareddy28@users.noreply.github.com>
2023-12-16 11:30:58 +0530

    #include<stdio.h> int main() {     int n,i,count=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0 || arr[i]==0)         {             count=count+1;         }     }     if(count==n)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Even_Array.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,count=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0 || arr[i]==0)
+        {
+            count=count+1;
+        }
+    }
+    if(count==n)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
